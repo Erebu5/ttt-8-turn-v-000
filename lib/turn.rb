ad7c7b0daa1bc7f0865376ac552ceb28966f226e
Jessica Mott <wigmore3.14@gmail.com>
2017-09-16 17:16:14 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -19,3 +19,14 @@ end
 def move(board, index, value = "X")
   board[index] = value
 end
+
+#POSITION AVAILABLE
+def position_taken(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
+    return true
+  end
+end
+
+    
